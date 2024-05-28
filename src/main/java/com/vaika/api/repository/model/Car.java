a3bc3426b42f45d062612c997bd13ac34358eb9f
Tombosoa <hei.nantenaina.4@gmail.com>
2024-05-28 16:54:19 +0300

    feat: create car, car's type, motor's type, image model

---
+++ src/main/java/com/vaika/api/repository/model/Car.java
@@ -0,0 +1,40 @@
+package com.vaika.api.repository.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Car {
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private double price;
+    private String model;
+    private String color;
+    private String power;
+    private int placeNumber;
+    private boolean pinned;
+    @ManyToOne
+    private CarType type;
+    @ManyToOne
+    private MotorType motorType;
+    @ManyToOne
+    private Brand brand;
+    @OneToMany(mappedBy = "car")
+    @JsonManagedReference
+    private List<Image> images ;
+}
---
+++ src/main/java/com/vaika/api/repository/model/Image.java
@@ -0,0 +1,24 @@
+package com.vaika.api.repository.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Entity
+@NoArgsConstructor
+public class Image {
+    @Id
+    private String id;
+    private String url;
+    @ManyToOne
+    @JsonBackReference
+    private Car car;
+}
