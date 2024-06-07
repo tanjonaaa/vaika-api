f352b442953f32b932aab19693075f6e178bdf97
Tendry24 <hei.tendry.2@gmail.com>
2024-06-08 02:41:41 +0300

    feat: CarType,MotorType::findAll

--- src/main/java/com/vaika/api/endpoint/rest/controller/MotorTypeController.java
+++ src/main/java/com/vaika/api/endpoint/rest/controller/MotorTypeController.java
@@ -3,7 +3,6 @@ package com.vaika.api.endpoint.rest.controller;
 import com.vaika.api.endpoint.rest.mapper.MotorTypeMapper;
 import com.vaika.api.endpoint.rest.model.MotorType;
 import com.vaika.api.service.MotorTypeService;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -11,18 +10,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @AllArgsConstructor
 public class MotorTypeController {
-  private final MotorTypeService motorTypeService;
-  private final MotorTypeMapper motorTypeMapper;
+    private final MotorTypeService motorTypeService;
+    private final MotorTypeMapper motorTypeMapper;
 
-  @GetMapping("/motor-type")
-  public List<MotorType> getAllMotorTypePaginate(
-      @RequestParam(required = false, defaultValue = "0") int page,
-      @RequestParam(required = false, defaultValue = "6") int size) {
-    Page<com.vaika.api.repository.model.MotorType> motorTypes =
-        motorTypeService.findAllMotorType(PageRequest.of(page, size));
-    return motorTypes.map(motorTypeMapper::toRest).toList();
-  }
+    @GetMapping("/motor-type")
+    public List<MotorType> getAllMotorTypePaginate(@RequestParam(required = false,defaultValue = "0") int page,
+                                                   @RequestParam(required = false,defaultValue = "6" ) int size){
+        Page<com.vaika.api.repository.model.MotorType> motorTypes =
+                motorTypeService.findAllMotorType(PageRequest.of(page,size));
+        return motorTypes.map(motorTypeMapper::toRest).toList();
+    }
 }
